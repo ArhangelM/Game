483d698f3af83b8b68d49e890442ddd988f40740
SMA <SMA@d.c>
2025-05-14 18:33:40 +0300

    Add player class

---
+++ Game/Player.cs
@@ -0,0 +1,24 @@
+﻿namespace Game
+{
+    internal class Player
+    {
+        public string Name { get; }
+        public int Age { get; }
+        public int NumberGames { get; }
+        public int NumberWins { get; }
+
+        public Player(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public void ShowStatistics()
+        {
+            Console.WriteLine($"Нiкнейм: {Name}");
+            Console.WriteLine($"Вік: {Age}");
+            Console.WriteLine($"Кiлькість зiграних iгор за сесiю: {NumberWins}");
+            Console.WriteLine($"Кiлькість перемог: {NumberWins}");
+        }
+    }
+}
